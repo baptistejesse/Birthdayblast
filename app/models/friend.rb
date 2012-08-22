166883ed11720f8e56adc7a39ea5ac34625fb578
baptistejesse <baptistejesse@yahoo.com>
2012-08-22 14:15:34 -0400

    again

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,13 +1,14 @@
 class Friend < ActiveRecord::Base
   belongs_to :user
   attr_accessible :birthday, :fb_id, :name
-  
+  after_save :month_day
 
 
 def month_day
+unless self.birthday.length > 5
 self.update_attributes(birthday: birthday.slice(0..4) ) 
 end
-
+end
 
 
 end
