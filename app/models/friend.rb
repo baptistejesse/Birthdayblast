b1790b0ff0710bd4add4bea28002605393975a98
baptistejesse <baptistejesse@yahoo.com>
2012-08-24 16:59:08 -0400

    again

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,12 +1,12 @@
 class Friend < ActiveRecord::Base
   belongs_to :user
   attr_accessible :birthday, :fb_id, :name
-  after_save :month_day
+
 
 
 def month_day
-unless self.birthday.length > 5
-@stuff = self.update_attributes(birthday: birthday.slice(0..4))
+
+@stuff = self.update_attributes(birthday: self.birthday.slice(0..4)) unless self.birthday.length > 5
 
 end
 end
