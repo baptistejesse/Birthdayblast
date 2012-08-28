1b0d8f749e3a816f31e2ed9daed711b00926e9d3
baptistejesse <baptistejesse@yahoo.com>
2012-08-28 00:32:42 -0400

    changed worker

--- app/models/ironworker.rb
+++ app/models/ironworker.rb
@@ -4,13 +4,12 @@ class HelloWorker < IronWorker::Base
 
 
     
-def run()
+def run(user,message)
 #get the friends check for birthdays if they have a birthday send the message from the user_id
 @c = Time.now
-@birth = self.friends.select do |x|
+@birth = user.friends.select do |x|
 if x.birthday == "#{c.month}/#{c.day}"  
-  
-  
+@graph.put_wall_post("#{message}", "tmiley") 
   
 end   
   
