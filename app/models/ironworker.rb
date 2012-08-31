9f3bbdd81742202b0bab944e42fb0ff9fb8fddea
baptistejesse <baptistejesse@yahoo.com>
2012-08-30 21:12:13 -0400

    again

--- app/models/ironworker.rb
+++ app/models/ironworker.rb
@@ -4,7 +4,7 @@ class HelloWorker < IronWorker::Base
 
 
     
-def initialize(user,message, linky="")
+def initialize(user)
 #get the friends check for birthdays if they have a birthday send the message from the user_id
 @user = user
 @message = message
@@ -12,7 +12,7 @@ def initialize(user,message, linky="")
 @c = Time.now
 @birth = @user.friends.select do |x|
 if x.birthday == "#{c.month}/#{c.day}"  
-@user.facebook { |fb| fb.put_wall_post("#{@message}",{name: x.name linky:@linky }, x.fb_id)}
+@user.facebook { |fb| fb.put_wall_post("#{@message}",{name: x.name, linky:@linky }, x.fb_id)}
 end   
   
 end
