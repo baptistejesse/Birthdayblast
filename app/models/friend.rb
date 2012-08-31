479988f79e3ec3adbd070e68bf394f9b8eb500e1
baptistejesse <baptistejesse@yahoo.com>
2012-08-30 23:01:02 -0400

    again

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -11,7 +11,7 @@ end
 end
 
 def pic_name
-  "#{image_path("https://graph.facebook.com/#{self.fb_id}/picture")}"  "#{self.name}"
+   "#{<img src="#{image_path("https://graph.facebook.com/#{self.fb_id}/picture")}"/>}"  "#{self.name}"
 end
 
 
