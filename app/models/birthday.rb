15555ee0b8343a9d26c436f9dfb9179d7e360e1a
baptistejesse <baptistejesse@yahoo.com>
2012-09-04 13:43:29 -0400

    again

--- app/models/birthday.rb
+++ app/models/birthday.rb
@@ -1,14 +1,14 @@
 class Birthday < ActiveRecord::Base
   attr_accessible :message
   belongs_to :user
+  after_create :set_message
 
 
-
-after_create do |message|
+def set_message
  #want to get user and get each friend id and set their message and link 
-@friends = Friend.where("fb_id = :fb_id AND user_id = :user_id", :fb_id => message.friend, :user_id => message.user_id)
-@friends.links = message.linky
-@friends.message = message.message
+@friends = Friend.where("fb_id = :fb_id AND user_id = :user_id", :fb_id => self.friend, :user_id => self.user_id)
+@friends.links = self.linky
+@friends.message = self.message
 @friends.save
 
 end
