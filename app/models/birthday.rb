434b7a424769fdc373ca62e40da52d1a08b1cc3a
baptistejesse <baptistejesse@yahoo.com>
2012-09-04 12:27:52 -0400

    again

--- app/models/birthday.rb
+++ app/models/birthday.rb
@@ -7,12 +7,12 @@ class Birthday < ActiveRecord::Base
 after_create do |message|
  #want to get user and get each friend id and set their message and link 
 @user = User.find_by_id(message.user_id)  
- @user.friends.select { |friends| 
-if friends.fb_id == message.friend
-friends.links = message.linky
-friends.message = message.message
-friends.save
-end
+@user.friends.select { |friends| 
+@friends = friends.where(fb_id: message.friend)
+@friends.links = message.linky
+@friends.message = message.message
+@friends.save
+
 }
 
 end
