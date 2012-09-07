e26ca13afdf6d8eff75f139f489cca74ef001fdc
baptistejesse <baptistejesse@yahoo.com>
2012-09-07 17:30:43 -0400

    again

--- app/models/birthday.rb
+++ app/models/birthday.rb
@@ -7,8 +7,9 @@ class Birthday < ActiveRecord::Base
 def set_message
  #want to get user and get each friend id and set their message and link 
 @friends = Friend.where("fb_id = :fb_id AND user_id = :user_id", :fb_id => self.friend, :user_id => self.user_id)
-@friends.update_attributes( spotify: self.linky, message: self.message)
 return @friends
+end
+@friends.update_attributes( spotify: self.linky, message: self.message)
 end
 
 
