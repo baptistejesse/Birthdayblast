1adcbbf7bf0757630bd81f2e16ac25aeeb7560d0
baptistejesse <baptistejesse@yahoo.com>
2012-09-07 21:40:59 -0400

    again

--- app/models/birthday.rb
+++ app/models/birthday.rb
@@ -1,12 +1,12 @@
 class Birthday < ActiveRecord::Base
   attr_accessible :message
   belongs_to :user
-  after_create :set_message
+  after_create :gogo
 
 
-def set_message
+def gogo
  #want to get user and get each friend id and set their message and link
-@user  = User.find_by_id(self.user_id)  
+@user = User.find_by_id(self.user_id)  
 @friend = @user.friends.where(fb_id: self.friend)
 @friend.spotify = self.linky,
 @friend.message = self.message
